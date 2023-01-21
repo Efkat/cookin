333c5942400d1e7c95bb58ccd03189c384fa4b0b
Hugo Benabdelhak <hugo.ben80000@gmail.com>
2023-01-21 21:44:59 +0100

    Send Recipes to IndexController.php

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Repository\RecipeRepository;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +11,13 @@ use Symfony\Component\Routing\Annotation\Route;
 class IndexController extends AbstractController
 {
     #[Route('/', name: 'index', methods: ['GET'])]
-    public function index(): Response
+    public function index(ManagerRegistry $registry): Response
     {
+        $recipeRepository = new RecipeRepository($registry);
+
         return $this->render('index/index.html.twig', [
-            'controller_name' => 'IndexController',
+            'title' => 'Acceuil',
+            'recipes' => $recipeRepository->findAll()
         ]);
     }
 }
