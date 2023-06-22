c64828ce71c1ca043f2a9246b01e749b87d0f6d6
Hugo Benabdelhak <hugo.ben80000@gmail.com>
2023-06-22 21:09:34 +0200

    Create Recipe Entity

---
+++ src/Entity/Recipe.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\RecipeRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: RecipeRepository::class)]
+class Recipe
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $Title = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $Summary = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $Picture = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $publishDate = null;
+
+    #[ORM\Column]
+    private ?int $cookingTime = null;
+
+    #[ORM\Column]
+    private ?int $preparationTime = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->Title;
+    }
+
+    public function setTitle(string $Title): static
+    {
+        $this->Title = $Title;
+
+        return $this;
+    }
+
+    public function getSummary(): ?string
+    {
+        return $this->Summary;
+    }
+
+    public function setSummary(string $Summary): static
+    {
+        $this->Summary = $Summary;
+
+        return $this;
+    }
+
+    public function getPicture(): ?string
+    {
+        return $this->Picture;
+    }
+
+    public function setPicture(string $Picture): static
+    {
+        $this->Picture = $Picture;
+
+        return $this;
+    }
+
+    public function getPublishDate(): ?\DateTimeInterface
+    {
+        return $this->publishDate;
+    }
+
+    public function setPublishDate(\DateTimeInterface $publishDate): static
+    {
+        $this->publishDate = $publishDate;
+
+        return $this;
+    }
+
+    public function getCookingTime(): ?int
+    {
+        return $this->cookingTime;
+    }
+
+    public function setCookingTime(int $cookingTime): static
+    {
+        $this->cookingTime = $cookingTime;
+
+        return $this;
+    }
+
+    public function getPreparationTime(): ?int
+    {
+        return $this->preparationTime;
+    }
+
+    public function setPreparationTime(int $preparationTime): static
+    {
+        $this->preparationTime = $preparationTime;
+
+        return $this;
+    }
+}
